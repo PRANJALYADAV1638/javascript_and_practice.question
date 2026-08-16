0f312a903136cab76fc6261693253a4b2c52324f
pranjal yadav <yashydv0677@gmail.com>
2026-08-16 12:41:04 +0530

    Chapter 1 with practice questions

---
+++ chapter1.js
@@ -0,0 +1,46 @@
+console.log(" welcome yash");
+console.log(" start learning ");
+console.log(" javascrpit");
+
+console.log("  learning variable  ");
+let name=" YASH YADAV "
+x=null;
+y=undefined;
+isfollow=true;
+age=24
+
+/*
+variable rule
+
+*  let const char
+let by deafult undefined
+not char
+
+* data types  
+primitive  
+non primitive
+
+
+constant ka object ki key ko change kr skta ha
+
+
+*/
+console.log(name);
+console.log(age);
+
+const student = {
+    fullName: "Yash Yadav",
+    cgpa: 8,
+    city: "Gurgaon"
+};
+
+
+
+//   PRACTICE  QUESTION
+
+const profile={
+    userName:"yash rao",
+    followers:31.7,
+    following:532,
+    isfollow: true
+};
